ee1d2e9577c14857f5e29491d5e5a455cd8ab534
Jerker Nyberg von Below <jerker.nyberg@gmail.com>
2019-03-08 15:57:19 +0100

    face

--- project/facedetect.py
+++ project/facedetect.py
@@ -2,3 +2,27 @@
 import cv2
 import numpy as np
 
+def __init__():
+    print "hello"
+
+def testing(n):
+    """Just a test for the testing to work"""
+    return n+1
+    
+def loadThumbnailImage():
+    """Load the thumbnail image"""
+    return
+
+def loadFullimage():
+    """Load the full image"""
+    return
+
+def findFace():
+    """Find the thumbnail in the full image and return coordinates"""
+    return
+
+def getPart(coordinates):
+    """Get the part of the full image back"""
+    return
+
+
